cb467c65ea62b0296b969faa5ec63de77007a60b
Eslam Ahmed <106440929+RXROW@users.noreply.github.com>
2024-06-22 11:31:47 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  burger.classList.toggle('toggle');
+  console.log("efgth");
+});
+
+//   By Eslam Ahmed Web Developer
+//  Email  => aslamalmswdya@gmail.com // Whats Up => 01012966375 
